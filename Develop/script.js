f77e3223c60e9335a1ec344a32a708c1a049b655
April H <aprilhunt00.ah@gmail.com>
2024-02-12 23:45:47 -0500

    Updated JS code

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,83 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+DOM element reference
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// Function to obtain user input for password options and password length
+function getPasswordOptions() {
+var length = parseInt(prompt('How many characters would you like your password to contain?'), 10);
 
+
+if (Number.isNaN(length) || length < 8 || length > 128) {
+alert('Password length must be a number between 8 and 128.');
+return null;
 }
 
-// Add event listener to generate button
+
+var hasSpecialCharacters = confirm('Include special characters?');
+var hasNumericCharacters = confirm('Include numeric characters?');
+var hasLowercaseCharacters = confirm('Include lowercase characters?');
+var hasUppercaseCharacters = confirm('Include uppercase characters?');
+
+
+if (!hasSpecialCharacters && !hasNumericCharacters && !hasLowercaseCharacters && !hasUppercaseCharacters) {
+alert('Password must include at least one character type.');
+return null;
+}
+
+
+var passwordOptions = {
+length: length,
+hasSpecialCharacters: hasSpecialCharacters,
+hasNumericCharacters: hasNumericCharacters,
+hasLowercaseCharacters: hasLowercaseCharacters,
+hasUppercaseCharacters: hasUppercaseCharacters
+};
+
+
+return passwordOptions;
+}
+
+
+// Function to get a random element from array
+function getRandom(arr) {
+var randIndex = Math.floor(Math.random() * arr.length);
+return arr[randIndex];
+}
+
+
+// Function to generate a password with given inputs
+function generatePassword() {
+var options = getPasswordOptions();
+
+
+var currentArr = [];
+if (options.hasSpecialCharacters) currentArr = currentArr.concat(specialCharacters);
+if (options.hasNumericCharacters) currentArr = currentArr.concat(numericCharacters);
+if (options.hasLowercaseCharacters) currentArr = currentArr.concat(lowerCasedCharacters);
+if (options.hasUppercaseCharacters) currentArr = currentArr.concat(upperCasedCharacters);
+
+
+var password = "";
+for (var i = 0; i < options.length; i++) {
+password += getRandom(currentArr);
+}
+
+
+return password;
+}
+
+
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
+}
+
+
 generateBtn.addEventListener("click", writePassword);
+
+
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Password Generator Starter Code
+# Watchword Generator Starter Code
+
+
+![img](//path here)
+
+
+## Description
+This is a browser based application for an employee to generate a random password with a specific criteria. This application features a polished UI and responsive HTML, CSS and Javascript to produce a secure password.
+
+
+## User Story
+As an employee with access to sensitive data, I want to randomly generate a password that meets certain criteria so that I can create a strong password that provides greater security.
+
+
+## Screenshot
+![Screenshot](path)
+This screenshot shows the password generator prior to the search criteria.
+
+
+![Screenshot2](path)
+This screenshot provides the options the user has.
+
+
+![Screenshot3](path)
+This screenshot shows an example of the developed file.
+
+
+## Deployed Application
+[Example: https://your-deployed-app-url.com]
